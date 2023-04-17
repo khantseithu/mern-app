c076d815d5b2da8927906889a143eb0e6453a5ae
Khant Sithu <khantseithuu@gmail.com>
2023-04-17 12:39:35 +0000

    get req tested with thunder client note: set port visibility to public(codespaces)

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,12 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.get("/api/goals", (req, res) => {
+  res.status(200).json({
+    message: "Hello from the server side!",
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
